deccfe5f26216279f9c2a41d0d9e6e1f3fd3efac
maaicondgl <maaicondgl@outlook.com>
2022-05-19 19:19:47 -0300

    Atv2

---
+++ chapterFS/chapterFS/Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿namespace chapterFS.Models
+{
+    public class Livro
+    {
+        public int id { get; set; }
+
+        public string? titulo { get; set; }
+
+        public int? QuantidadePaginas { get; set; }
+
+        public bool Disponivel { get; set; }
+    }
+}
---
+++ chapterFS/chapterFS/Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using chapterFS.Contexts;
+using chapterFS.Models;
+
+namespace chapterFS.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+        public LivroRepository(ChapterContext context)
+        {
+           _context = context;
+        }
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+    }
+}
